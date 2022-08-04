0944f4da67a88ff67e4650d74f5cb5d2c641f152
Parkil <alkain77@gmail.com>
2022-08-04 18:05:32 +0900

    임시업로드

---
+++ src/main/java/com/example/bootthymeleaf/config/AppConfig.java
@@ -0,0 +1,21 @@
+package com.example.bootthymeleaf.config;
+
+import lombok.RequiredArgsConstructor;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.UrlTemplateResolver;
+
+@RequiredArgsConstructor
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.addTemplateResolver(new UrlTemplateResolver());
+        templateEngine.addDialect(new LayoutDialect());
+        return templateEngine;
+    }
+}

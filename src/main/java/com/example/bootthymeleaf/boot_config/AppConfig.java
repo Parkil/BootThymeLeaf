360cc7bc89d466fbc0d74c01d8c6c48bfa04fa32
Parkil <alkain77@gmail.com>
2022-08-11 16:31:16 +0900

    spring security 정보를 thymeleaf에서 표시할수 있도록 설정수정

--- src/main/java/com/example/bootthymeleaf/boot_config/AppConfig.java
+++ src/main/java/com/example/bootthymeleaf/boot_config/AppConfig.java
@@ -1,14 +1,18 @@
 package com.example.bootthymeleaf.boot_config;
 
-import lombok.RequiredArgsConstructor;
 import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
-@RequiredArgsConstructor
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
+    @Bean
+    public SpringSecurityDialect springSecurityDialect() {
+        return new SpringSecurityDialect();
+    }
+
     @Bean
     public LayoutDialect layoutDialect() {
         return new LayoutDialect();
